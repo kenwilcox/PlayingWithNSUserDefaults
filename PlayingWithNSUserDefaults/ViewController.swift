30a65f55ac2d1dbddc64e1bd3cb4d990dc2637c0
Ken Wilcox <wilcoxk@gmail.com>
2016-01-01 20:51:36 -0700

    integral types

--- PlayingWithNSUserDefaults/ViewController.swift
+++ PlayingWithNSUserDefaults/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setInteger(25, forKey: "Age")
+        defaults.setBool(true, forKey: "UseTouchId")
+        defaults.setDouble(M_PI, forKey: "Pi")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
